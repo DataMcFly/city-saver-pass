6e75b8f981f1b88289f5d2a2b1f4590b2fbfbbbe
Roger Stringer <roger@datamcfly.com>
2016-07-22 15:07:46 -0400

    vendors and coupons II

--- app/home/home.js
+++ app/home/home.js
@@ -1,65 +1,38 @@
 angular.module('MainCtrl', ['ngRoute'])
-.controller('MainController', function($scope,$timeout,$location, coupons,Coupon, vendors, Vendor) {
-	$scope.coupons = coupons;
-	$scope.vendors = vendors;
-
+.controller('MainController', function($scope,$timeout,$location, Login, me) {
 	$scope.tagline = 'To the moon and back!';	
 
 	$scope.goToPage = function( page ){
 		$location.path(page);
 	}
-/*
-	var Ref = Coupon.flybase();
-	
-	Ref.on('added', function( data ){
-		$timeout(function() {
-			$scope.coupons.push( data.value() );
-		});
-	});
-	Ref.on('changed', function( data ){
-		$timeout(function() {
-			var snapshot = data.value();
-			for( i in $scope.coupons ){
-				var coupon = $scope.coupons[ i ];
-				if( coupon._id == snapshot._id ){
-					$scope.coupons[ i ] = snapshot;
-				}
-			}
-		});
-	});
-	Ref.on('removed', function( data ){
-		$timeout(function() {
-			var snapshot = data.value();
-			for( i in $scope.coupons ){
-				var coupon = $scope.coupons[ i ];
-				if( coupon._id == snapshot._id ){
-					$scope.coupons.splice(i, 1);
-				}
-			}
-		});
-	});
-*/
 }).controller('CouponCtrl', function($scope, $location, coupon) {
 	$scope.coupon = coupon;
 }).controller('VendorCtrl', function($scope, $location, vendor) {
 	$scope.vendor = vendor;
 }).config(['$routeProvider','$locationProvider', function ($routeProvider,$locationProvider) {
 	$routeProvider.when('/', {
-		templateUrl: 'app/home/home.html?v=1.a',
+		templateUrl: 'app/home/home.html?v=1.b',
 		controller: 'MainController',
 		resolve:{
-			coupons:function(Coupon){
-//				return Coupon.query({"tag":{"$not":"1"}},{"limit":10});
-				return Coupon.all();
-			}
+			login:function( Login ){
+				return new Login();
+			},
+			me:function(User, Login){
+				var login = new Login();
+				if( login.isLoggedIn() ){
+					var token = login._getToken();
+					var u = User.getById(token);
+					return u;
+				}
+			} 			
 		}
 	}).when('/coupon/:id', {
 		templateUrl: 'app/home/coupon.html?a=1',
 		controller: 'CouponCtrl',
 		resolve:{
 			coupon:function(Coupon, $route){
-				var p = Coupon.getById($route.current.params.id);
-				return p;
+				var c = Coupon.getById($route.current.params.id);
+				return c;
 			} 
 		}
 	}).when('/vendor/:id', {
@@ -67,8 +40,8 @@ angular.module('MainCtrl', ['ngRoute'])
 		controller: 'VendorCtrl',
 		resolve:{
 			vendor:function(Vendor, $route){
-				var p = Vendor.getById($route.current.params.id);
-				return p;
+				var v = Vendor.getById($route.current.params.id);
+				return v;
 			} 
 		}
 	});	

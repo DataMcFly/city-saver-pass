f905f71e2d80c66e726fe3c53746ff542078028d
Roger Stringer <roger@datamcfly.com>
2016-07-25 13:40:39 -0400

    test

--- app/config.js
+++ app/config.js
@@ -6,11 +6,14 @@ angular.module('myApp.config', [])
 .constant('loginRedirectPath', '/login')
 .constant('SERVER_CONFIG', {	//	for talking to local API
 	url: '/api'
-})
-.constant('FLYBASE_CONFIG',{	API_KEY:'token:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlrZXkiOiI1ZjA1MzQ0OS0wODJhLTQ4ZTUtOGIxYy01ZTk2NjBlYzFlYTgiLCJpYXQiOjE0NjgzNzUxOTUsImNyZWF0ZWQiOiIyMDE2LTA3LTEyIDE4OjU5OjU1IiwiZXhwaXJlcyI6IjIwMTYtMDgtMTEgMTg6NTk6NTUifQ.0-C9mkJ91iSyxS7BqSfda4KRYNMnd-br4g3Mmgd79_E', 
+}).constant('FLYBASE_CONFIG',{		API_KEY:'token:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlrZXkiOiI1ZjA1MzQ0OS0wODJhLTQ4ZTUtOGIxYy01ZTk2NjBlYzFlYTgiLCJpYXQiOjE0NjgzNzUxOTUsImNyZWF0ZWQiOiIyMDE2LTA3LTEyIDE4OjU5OjU1IiwiZXhwaXJlcyI6IjIwMTYtMDgtMTEgMTg6NTk6NTUifQ.0-C9mkJ91iSyxS7BqSfda4KRYNMnd-br4g3Mmgd79_E', 
 	DB_NAME:'citysaverpass'
 })
-// double check that the app has been configured before running it and blowing up space and time
+// speed up angular by disabling debug...
+.config(['$compileProvider', function ($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}])
+// double check that the app has been configured before running it and blowing up space and time...
 .run(['FLYBASE_CONFIG', '$timeout', function(FLYBASE_CONFIG, $timeout) {
 	if( FLYBASE_CONFIG.API_KEY.match('YOUR-API-KEY') ) {
 		angular.element(document.body).html('<div class="container"><h1>Please configure <code>app/config.js</code> before running!</h1></div>');
